0752f05275ebb8fbcc2cdd2f83f02945581edfcc
BjarkiThorJons <bjarkithorjonsson@gmail.com>
2019-03-05 12:25:27 +0000

    Update skilaverk5.py

--- skilaverk5.py
+++ skilaverk5.py
@@ -1,3 +1,4 @@
+import math
 class Node:
     def __init__(self, d):
         self.data = d
@@ -86,6 +87,50 @@ class DLL: # DLL = Dobule Linked List
         else:
             return self.head.delete(d)
 
+
+class Vigur:
+
+    # Smiður frumstillir x og y hnit vigurs eftir parametrum
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    # Fall sem skrifar hnit vigurs á skjá
+    def prenta(self):
+        print(self.x,",",self.y)
+
+    # Fall sem skilar lengd
+    def lengd(self):
+        return(round(math.sqrt((self.x**2)+(self.y**2))))
+    # Fall sem skilar hallatölu
+    def halli(self):
+        return(self.y/self.x)
+    # Fall sem skilar þvervigri
+    def þvervigur(self):
+        return Vigur(-self.y,self.x)
+
+    # Fall sem skilar stefnuhorni
+    def stefnuhorn(self):
+        t = math.degrees(math.atan(self.y/self.x))
+        if self.x > 0:
+            return t
+        elif self.x < 0:
+            if self.y > 0:
+                return 180+t
+            else:
+                return t-180
+
+    # Fall sem tekur vigur sem parameter og skilar horni milli vigra
+    def horn(self,v):
+        a = math.sqrt((self.x**2)+(self.y**2))
+        b = math.sqrt((v.x**2)+(v.y**2))
+        ab = (self.x * v.x) + (self.y * v.y)
+        h = math.acos(ab/(a*b))
+        return math.degrees(h)
+    # Fall sem tekur vigur sem parameter og skilar summu vigri
+    def summa(self,v):
+        return Vigur(self.x+v.x,self.y+v.y)
+print("----LINKED LISTI----")
 dbl = DLL()
 dbl.append(5)           # 5
 dbl.append(7)           # 5 7
@@ -98,3 +143,19 @@ print(dbl.delete(10))   # 0 1 5 7
 dbl.printList()
 print(dbl.find(5))      # True
 print(dbl.find(10))     # False
+
+# Keyrsluforrit
+print("----VIGUR----")
+v1 = Vigur(-3,-3)
+v1.prenta()
+print("Lengd: ", v1.lengd())
+print("Halli: ", v1.halli())
+vþ = v1.þvervigur()
+print("Þvervigur: " , end=" ")
+vþ.prenta()
+print("Stefnuhorn: ", v1.stefnuhorn())
+v2 = Vigur(-3,1)
+print("Horn milli vigra: " , v2.horn(v1))
+v3 = v1.summa(v2)
+print("Summa: " , end=" ")
+v3.prenta()
