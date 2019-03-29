c967a72d4575e8480cfc4d9f634969eab827990a
BjarkiThorJons <bjarkithorjonsson@gmail.com>
2019-03-29 14:04:56 +0000

    Update Memoization.py

--- Memoization.py
+++ Memoization.py
@@ -1,17 +1,23 @@
 #Bjarki Þór Jónsson
 k = {}
-f = open("triangle.txt","r")
+f = open("triangle2.txt","r")
 listi = f.read().split("\n")
 for x in range(len(listi)):
    listi[x] = listi[x].split(" ")
 print(listi)
 def path(listi,ts,ls):
+    key = str(ts)+","+str(ls)
+    if key in k:
+        return k[key]
     if len(listi)-2 == ls:
-        print(int(listi[ls+1][ts+1]), int(listi[ls+1][ts]))
-        return int(listi[ls][ts]) + max(int(listi[ls+1][ts+1]), int(listi[ls+1][ts]))
+        gildi = int(listi[ls][ts]) + max(int(listi[ls+1][ts+1]), int(listi[ls+1][ts]))
+        k[key] = gildi
+        return gildi
     else:
         s1 = path(listi,ts,ls+1)
         s2 = path(listi,ts+1,ls+1)
-        return int(listi[ls][ts]) + max(s1,s2)
+        gildi = int(listi[ls][ts]) + max(s1,s2)
+        k[key] = gildi
+        return gildi
 s = path(listi,0,0)
 print(s)
